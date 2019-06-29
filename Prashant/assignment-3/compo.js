b322586d4adefdca19c19ca4d00bc237785c3158
18JE0618 <50778193+18JE0618@users.noreply.github.com>
2019-06-29 16:00:04 +0530

    Add files via upload

---
+++ Prashant/assignment-3/compo.js
@@ -0,0 +1,12 @@
+// LOAD BG IMAGE
+const BG_IMG = new Image();
+BG_IMG.src = "bg.jpg";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "lifes.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "Points.png";
