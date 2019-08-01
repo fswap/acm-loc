759683e7314a11c676f6cae72e0cc9ca2daeccad
18JE0618 <50778193+18JE0618@users.noreply.github.com>
2019-08-01 20:54:04 +0530

    Create index.php

---
+++ Prashant/project 2/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>IIT ISM LIBRARY</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">IIT ISM LIBRARY</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="lib.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="reg.php">Register</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="main" class="container-fluid" style="margin-top: 70px;">
+  <div class="row">
+    <div class="col">
+       <div class="card img-fluid" style="width:100%">
+    <img class="card-img-top" src="lib2.jpg" alt="Card image" style="width:100%">
+    <div class="card-img-overlay">
+      <h4 class="card-title">WELCOME TO CENTRAL LIBRARY</h4>
+      <p class="card-text"></p>
+      <a href="adminlogin.php" class="btn btn-primary">Login</a>
+    </div>
+  </div>
+    </div>
+   
+  </div>
+  <hr class="w-25 mx-auto"> 
+  <div id="aboutt" class="row">
+    <div class="col">
+      <h2 id="about">About</h2>
+      <p>The Central Library of Indian Institute of Technology (Indian School of Mines) Dhanbad is the heart of the institute providing direct academic and research supports to all departments.</p>
+
+<p>The Central Library is an automated library in terms of records organisation and management of all its different sections, search and discovery, information retrieval and service delivery. The whole library operations run on LIBSYS integrated library management software. Over the years the library has developed several databases of books, journals, theses/ dissertations, conference proceedings, etc. All documents have been bar-coded. Focus has been given on collection build-up of print resources as well as licensed e-resources plus E-Shodh Sindhu sponsored resources. The library has reached all the departments, hostels, and quarters on the campus wide LAN.</p>
+
+<p>The library has initiated the development of the “Institutional Digital Repository” based on DSpace open source platform. The Central Library has set up its “Research Documentation Cell” based on the research outputs which are indexed in Scopus and Web of Science.</p>
+
+<p>To meet the challenges of increasing number of students, research scholars and faculty members as well as ever changing information and communication technologies a G+7 centrally air-conditioned library having state-of-the-art facilities is being developed to provide top quality library services.</p>
+
+<p>The staff of the library strive their best to help push the boundaries of IIT (ISM) Dhanbad in national and international spaces.</p>
+    </div>
+  </div>
+</div>
+<script src="lib.js"></script> 
+</body>
+</html>
